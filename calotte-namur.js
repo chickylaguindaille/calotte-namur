1ba59649057392ac2f954463d9498a133a2765f3
Etienne Leroy <geti.leroy@gmail.com>
2022-03-06 16:13:11 +0100

    transfert

---
+++ calotte-namur.js
@@ -0,0 +1,6 @@
+let accueil = document.getElementById("accueil");
+
+accueil.addEventListener("click", () => {
+    console.log("test")
+    document.location.href="/Applications/MAMP/htdocs/FIND/Calotte-Namur/index.html";
+})
